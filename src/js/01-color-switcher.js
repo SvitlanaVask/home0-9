6c15bb848ec46abe5d4ed2ccf38f97913ea19969
SvitlanaVas <svetaklymchuk@gmail.com>
2023-06-06 19:31:58 +0300

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const COLOR_DELAY = 1000;
+let bodyStyle = document.body.style;
+let intervalId = null;
+  
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
 
+function onStartBtnClick() {
+  console.log('hello')
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyStyle.backgroundColor = getRandomHexColor();
+  }, COLOR_DELAY);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
