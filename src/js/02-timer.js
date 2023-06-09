238f2e19fe2c8a31e6494115c02f3fc16cc8167e
SvitlanaVas <svetaklymchuk@gmail.com>
2023-06-09 19:25:38 +0300

    add 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,34 +1,131 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
+import {Notify} from 'notiflix';
 
+const pickerInput = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const msgRef = document.querySelector('.message')
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minsRef = document.querySelector('[data-minutes]');
+const secsRef = document.querySelector('[data-seconds]');
+
+pickerInput.disabled = false;
+startBtn.disabled = true;
+stopBtn.disabled = true;
+
+// Обєкт налаштувань для flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
+  // Метод бібліотеки flatpickr
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const pickedDate = selectedDates[0].getTime();
+
+    if (pickedDate <= Date.now()) {
+      Notify.warning('Please, choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      stopBtn.disabled = false;
+    }
+    // Повертає в обєкт timer обрану дату pickedDate
+    timer.setTargetDate(pickedDate);
   },
 };
 
-flatpickr("#datetime-picker", options);
+//Ініціалізація бібліотеки
+flatpickr('#datetime-picker', options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+//Таймер зворотного відліку
+timer = {
+  isActive: false,
+  intervalId: null,
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // при зверненні this.targetDate в методі start() отримаємо вибрану дату
+  setTargetDate(targetDate) {
+    this.targetDate = targetDate;
+  },
 
-  return { days, hours, minutes, seconds };
+  start() {
+    if (this.isActive) {
+      return
+    }
+    
+    this.intervalId = setInterval(() => {
+      const deltaTime = this.targetDate - Date.now();
+
+      if (deltaTime < 1000) {
+        const time = this.convertMs(0);   
+        updateTimerFace(time);
+
+        msgRef.textContent = `Timer stopped on ${new Date()}`;
+
+        return;
+      } else {
+        const time = this.convertMs(deltaTime);
+        updateTimerFace(time);
+
+        pickerInput.disabled = true;
+        startBtn.disabled = true; 
+      }
+      
+      stopBtn.disabled = false;
+    }, 1000);
+        
+    this.isActive = true;
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    
+    const time = this.convertMs(0);   
+    updateTimerFace(time);
+
+    pickerInput.disabled = false;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  },
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+  },
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
 }
+
+startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+stopBtn.addEventListener('click', () => {
+  timer.stop();
+});
+
+function updateTimerFace({ days, hours, minutes, seconds}) {
+  daysRef.textContent = days;
+  hoursRef.textContent = hours;
+  minsRef.textContent = minutes;
+  secsRef.textContent = seconds;
+};
